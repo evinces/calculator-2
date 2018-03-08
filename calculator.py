e764bff17f3a3ab32f6b063a8a6496901cdcd85d
Hackbright Student <no-reply@hackbrightacademy.com>
2018-03-07 17:33:03 -0800

    Replaced list arguments with reduce()

--- calculator.py
+++ calculator.py
@@ -4,8 +4,8 @@ Using the arithmetic.py file from Calculator Part 1, create the
 calculator program yourself in this file.
 """
 
-from arithmetic import *
-# from arithmetic2 import *
+# from arithmetic import *
+from arithmetic2 import *
 
 
 # Your code goes here
@@ -25,16 +25,16 @@ while True:
 
         try:
             if tokens[0] == "+":
-                print add(tokens[1:])
+                print reduce(lambda x, y: add(x, y), tokens[1:])
 
             elif tokens[0] == "-":
-                print subtract(tokens[1:])
+                print reduce(lambda x, y: subtract(x, y), tokens[1:])
 
             elif tokens[0] == "*":
-                print multiply(tokens[1:])
+                print reduce(lambda x, y: multiply(x, y), tokens[1:])
 
             elif tokens[0] == "/":
-                print divide(tokens[1:])
+                print reduce(lambda x, y: divide(x, y), tokens[1:])
 
             elif tokens[0] == "square":
                 print square(tokens[1])
@@ -43,10 +43,10 @@ while True:
                 print cube(tokens[1])
 
             elif tokens[0] == "pow":
-                print power(tokens[1], tokens[2])
+                print reduce(lambda x, y: power(x, y), tokens[1:])
 
             elif tokens[0] == "mod":
-                print mod(tokens[1], tokens[2])
+                print reduce(lambda x, y: mod(x, y), tokens[1:])
 
             else:
                 print "Invalid command: Please try again."
